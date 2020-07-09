d3acc54e0d2920d5b3a8cff0152e9d49e4bdf347
Kartik Kapur <kapurkartik@berkeley.edu>
2020-07-09 13:40:51 -0700

    landscape and also made a scoring functionality and ability to only answer once

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.java
@@ -1,36 +1,57 @@
 package com.bignerdranch.android.geoquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewDebug;
 import android.widget.Button;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "QUIZACTIVITY";
+    private static final String KEY_INDEX = "index";
+    private static final String KEY_INCORRECT = "incorrect";
+    private static final String KEY_CORRECT = "correct";
+
+
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mNextButton;
     private ImageButton mPreviousButton;
     private TextView mQuestionTextView;
 
+    private ArrayList<Integer> mCorrect = new ArrayList<>();
+    private ArrayList<Integer> mIncorrect = new ArrayList<>();
+
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_asia, true),
             new Question(R.string.question_australia, true),
             new Question(R.string.question_mideast, false)
-        };
+    };
     private int mCurrentIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+
         setContentView(R.layout.activity_main);
 
+        if(savedInstanceState!=null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mCorrect = savedInstanceState.getIntegerArrayList(KEY_CORRECT);
+            mIncorrect = savedInstanceState.getIntegerArrayList(KEY_INCORRECT);
+        }
+
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        updateQuestion();
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -48,19 +69,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mNextButton = (ImageButton) findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener(){
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex+1)%mQuestionBank.length;
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
             }
         });
 
         mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
-        mPreviousButton.setOnClickListener(new View.OnClickListener(){
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCurrentIndex = Math.abs(mCurrentIndex-1)%mQuestionBank.length;
+                if(mCurrentIndex < 0 ) {
+                    mCurrentIndex = mQuestionBank.length - 1;
+                } else {
+                    mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                }
                 updateQuestion();
             }
         });
@@ -69,31 +94,91 @@ public class MainActivity extends AppCompatActivity {
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex+1)%mQuestionBank.length;
+                mCurrentIndex = (mCurrentIndex + 1)%mQuestionBank.length;
                 updateQuestion();
             }
         });
 
-
-        }
-
-
-    private void updateQuestion( ){
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
+        updateQuestion();
     }
 
-    private void checkAnswer(boolean userPressedTrue){
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+        outState.putIntegerArrayList(KEY_CORRECT, mCorrect);
+        outState.putIntegerArrayList(KEY_INCORRECT, mIncorrect);
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+        if(mCorrect.contains(mCurrentIndex) || mIncorrect.contains(mCurrentIndex)) {
+            mTrueButton.setVisibility(View.GONE);
+            mFalseButton.setVisibility(View.GONE);
+        } else {
+            mTrueButton.setVisibility(View.VISIBLE);
+            mFalseButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId = 0;
         if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
-        } else{
+            mCorrect.add(mCurrentIndex);
+        } else {
             messageResId = R.string.incorrect_toast;
+            mIncorrect.add(mCurrentIndex);
+        }
+        updateQuestion();
+        Toast t = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
+        t.setGravity(Gravity.TOP, 0, 0);
+        t.show();
+        calculateScore();
+    }
+
+    private void calculateScore(){
+        Log.d(TAG, Integer.toString(mCorrect.size() + mIncorrect.size()));
+        if (mCorrect.size() + mIncorrect.size() == mQuestionBank.length){
+            Toast t = Toast.makeText(this,
+                    "You got " + Integer.toString(mCorrect.size()) + "/" +Integer.toString(mQuestionBank.length)+ " correct",
+                    Toast.LENGTH_LONG);
+            t.setGravity(Gravity.TOP, 0, 0);
+            t.show();
         }
 
-        Toast t = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
-        t.setGravity(Gravity.TOP, 0,0);
-        t.show();
     }
 }
