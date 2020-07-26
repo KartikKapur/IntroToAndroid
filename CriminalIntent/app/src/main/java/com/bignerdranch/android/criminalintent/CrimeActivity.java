5af080eef60a770312a0df640dcb2a03953c343e
Kartik Kapur <kapurkartik@berkeley.edu>
2020-07-25 18:13:20 -0700

    Experimented with auto generating fragments

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -8,18 +8,9 @@ import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class CrimeActivity extends FragmentActivity {
-    private final static String TAG = "CRIME_ACTIVITY";
+public class CrimeActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if (fragment == null){
-            fragment = new CrimeFragment();
-            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
-        }
+    protected Fragment createFragment(){
+        return new CrimeFragment();
     }
 }
